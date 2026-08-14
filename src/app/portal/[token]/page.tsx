2d193a80f3662a5da7fb8601d878f3b71537ec1a
b0bbyyyyy-pixel <b0bbyyyyy@aol.com>
2026-08-14 15:53:23 -0400

    Bug Fixes
    Bug Fixes

--- src/app/portal/[token]/page.tsx
+++ src/app/portal/[token]/page.tsx
@@ -20,7 +20,6 @@ export default async function PortalPage({
     .from('client_offer_portals')
     .select('*')
     .eq('token', token)
-    .neq('is_active', false)
     .single();
 
   if (error || !data) {
